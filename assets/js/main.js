70ba54f716e2cc0322efb83e87a6a923c8ec26ed
di-ja <dirk@mousezone.de>
2020-07-08 21:33:02 +0200

    JS Counter

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+let ergebnis = 0
+function plus1() {
+    ergebnis = ergebnis + 1
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function minus1() {
+    ergebnis = ergebnis - 1
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function plus10() {
+    ergebnis = ergebnis +10
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function minus10() {
+    ergebnis = ergebnis -10
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function plus100() {
+    ergebnis = ergebnis +100
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function minus100() {
+    ergebnis = ergebnis -100
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function reset() {
+    ergebnis = 0
+    document.getElementById("result").innerHTML = ergebnis 
+}
+function multi2() {
+    ergebnis = ergebnis *2
+    document.getElementById("result").innerHTML = ergebnis 
+}
